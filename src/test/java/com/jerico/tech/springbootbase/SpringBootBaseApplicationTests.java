341f73f3940231f7171cfab1ff8f29ac25561b87
hairui1986sd <hairui1986sd@163.com>
2023-04-16 23:40:40 +0800

    初始工程

---
+++ src/test/java/com/jerico/tech/springbootbase/SpringBootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerico.tech.springbootbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
